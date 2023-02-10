fcf51e3041864d82a9c705d3d241c4dec77d8bd0
Samuel Ian <osamwelian3@gmail.com>
2023-02-10 12:56:58 +0300

    Add api call to send phone number for transaction

--- payment_service_client/src/App.js
+++ payment_service_client/src/App.js
@@ -1,7 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+  const [phone_number, setPhone] = useState('');
+  const [message, setMessage] = useState('')
+  const [loading, setLoading] = useState(false);
+
+  const axiosInstance = axios.create({
+      baseURL: `http://localhost:8000`,
+      headers: {
+          'Accept': 'application/json',
+          'Content-type': 'application/json',
+      }
+  });
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setMessage('')
+    setLoading(true);
+    console.log(phone_number)
+
+    const body = JSON.stringify({phone_number})
+    
+    await axiosInstance({url:`/payment/pay/`, method: 'POST', data: body})
+    .then((res) => {
+      console.log(res.data)
+      setLoading(false);
+      setPhone('')
+    })
+    .catch((error) => {
+      console.log(error)
+      setMessage(error.message)
+      setLoading(false)
+    });
+
+  };
+
+
   return (
     <>
       <nav>
@@ -16,14 +56,17 @@ function App() {
       </nav>
       <section className='main'>
         <div className="box">
-          <form action="#" className="stkform">
+          <form onSubmit={handleSubmit} className="stkform">
             <h3>Make Payment</h3>
             <hr />
             <div>
-              <input className='input-field text-center' type="tel" id='phone' name='phone' minLength='10' placeholder='Enter your Mpesa Number' required />
+              <input className='input-field text-center' type="tel" id='phone' name='phone' minLength='10' value={phone_number} onChange={e => setPhone(e.target.value)} placeholder='Enter your Mpesa Number' required />
             </div>
             <div className='push'>
-              <button>Pay</button>
+              <button disabled={loading}>{loading ? <FontAwesomeIcon icon={faSpinner} spin /> : 'Pay'}</button>
+            </div>
+            <div>
+              {message == '' ? '' : <p className='error'>{message}</p>}
             </div>
           </form>
         </div>
