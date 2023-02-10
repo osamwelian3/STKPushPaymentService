fccbac6b5fdadff1f233dd7d3979020b8cc077c9
Samuel Ian <osamwelian3@gmail.com>
2023-02-10 23:18:26 +0300

    ft: Add stk push feature

--- payment_service_api/payment/views.py
+++ payment_service_api/payment/views.py
@@ -6,6 +6,7 @@ from .serializers import PaymentSerializer, PhoneSerializer
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from .transaction_handler import Transaction
 
 # Create your views here.
 class PaymentViewSet(viewsets.ModelViewSet):
@@ -16,15 +17,27 @@ class PaymentViewSet(viewsets.ModelViewSet):
     def pay(self, request, *args, **kwargs):
         if request.method == 'POST':
             phone = request.POST.get('phone_number') if request.POST.get('phone_number') is not None else request.data.get('phone_number')
-            return Response({'post_success': phone})
-        return Response({'error': 'failed'})
-        # user = self.get_object()
-        # serializer = PasswordSerializer(data=request.data)
-        # if serializer.is_valid():
-        #     user.set_password(serializer.validated_data['password'])
-        #     user.save()
-        #     return Response({'status': 'password set'})
-        # else:
-        #     return Response(serializer.errors,
-        #                     status=status.HTTP_400_BAD_REQUEST)
+            transaction = Transaction()
+            tr = transaction.stk_push(phone=phone, amount=1)
+            if 'errorMessage' in tr:
+                return Response({'error': tr['errorMessage']},status=400)
+            if 'ResponseCode' in tr:
+                if tr['ResponseCode'] == '0':
+                    validation = transaction.query_transaction(tr['CheckoutRequestID'])
+                    print(validation)
+                    while 'errorMessage' in validation:
+                        validate = transaction.query_transaction(tr['CheckoutRequestID'])
+                        if 'ResultDesc' in validate:
+                            if validate['ResultDesc'] == "The initiator information is invalid.":
+                                message = "Something went wrong"
+                                return Response({'error': message}, status=400)
+                            if validate['ResultDesc'] == "Request cancelled by user":
+                                message = "You took too long or cancelled the PIN request for the transaction Please try again"
+                                return Response({'error': message}, status=400)
+                            if validate['ResultDesc'] == "The service request is processed successfully.":
+                                message = "Yey... Payment went through"
+                                return Response({'success', message})
+                            
+            return Response({'error': 'Something went wrong'})
+        return Response({'error': 'Something went wrong'})
 
